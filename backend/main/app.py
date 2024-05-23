9f5cf8870dcd71ee47929020b1a6abc5c259f4a6
german <kochgerm@gmail.com>
2024-05-23 23:05:25 +0300

    procfile fixed

--- backend/main/app.py
+++ backend/main/app.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/endpoint', methods=['GET'])
 def endpoint():
     return jsonify({"message": "Hello, World!"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
