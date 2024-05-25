a093f6012e6ad9f58695466ed07972482b9787a0
Anatoly Efimov <efimo.tolik@gmail.com>
2024-05-26 00:46:56 +0400

    Basic chart render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import { Chart } from '@/components/Chart'
+import { useEffect, useState } from 'react'
+
+import api, { Point } from '@/api'
+
+import { Chart } from './components/ChartArm'
 
 import './App.css'
 
 function App() {
-	return <Chart />
+	const [responseSeries, setResponseSeries] = useState<Point[]>([])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const { data } = await api.getSeries('RESPONSE')
+			setResponseSeries(data)
+		}
+
+		fetchData()
+	}, [])
+	return <Chart data={responseSeries.slice(100, 3000)} />
 }
 
 export default App
