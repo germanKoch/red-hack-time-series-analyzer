8d509d69d0d7f2fdeee30025d5c02915b4faeef8
Anatoly Efimov <efimo.tolik@gmail.com>
2024-05-25 17:36:43 +0400

    GH pages init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
+export default defineConfig(({ command }) => {
+	const config: UserConfig = {
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, './src'),
+			},
+		},
+	}
+
+	if (command !== 'serve') {
+		config.base = '/red-hack-time-series-analyzer/'
+	}
+
+	return config
 })
